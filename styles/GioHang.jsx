1979c150198a04f9a2423d3c8889741311be4d66
pctcyber <pct.hcmut@gmail.com>
2021-03-04 01:14:19 +0700

    change className and Gio hang

--- styles/GioHang.jsx
+++ styles/GioHang.jsx
@@ -4,7 +4,7 @@ export default class GioHang extends Component {
     render() {
         return (
             <div>
-                
+                console.log(em gi oi);
             </div>
         )
     }
