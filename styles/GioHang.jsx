9aa7cb70f4b6038fbbe528e8e753775096d85cfb
pctcyber <pct.hcmut@gmail.com>
2021-03-04 11:24:52 +0700

    khoi tao bien x

--- styles/GioHang.jsx
+++ styles/GioHang.jsx
@@ -8,6 +8,7 @@ export default class GioHang extends Component {
                 console.log(co giang tinh);
                 console.log(ng thu 2);
                 console.log(ng thu 4);
+                var x = 2;
                 </div>
         )
     }
