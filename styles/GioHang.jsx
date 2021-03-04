5257f5bc292819eddda68638d3ca1e88438da419
pctcyber <pct.hcmut@gmail.com>
2021-03-04 11:29:16 +0700

    change variable x and create new variable y

--- styles/GioHang.jsx
+++ styles/GioHang.jsx
@@ -8,8 +8,8 @@ export default class GioHang extends Component {
                 console.log(co giang tinh);
                 console.log(ng thu 2);
                 console.log(ng thu 4);
-                var x = 2;
-                
+                var x = 3;
+                var y = 4;
                 </div>
         )
     }
